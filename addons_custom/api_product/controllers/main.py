47259a92b9e4be4d83c7d653814be9a5fa9a1469
hieu.duong <hieu.duong@icomm-group.com>
2025-04-01 14:28:40 +0700

    update

--- addons_custom/api_product/controllers/main.py
+++ addons_custom/api_product/controllers/main.py
@@ -1,7 +1,7 @@
 import logging
 from odoo import http
 from odoo.http import request
-from odoo.addons.jwt_provider.JwtRequest import jwt_request
+from ..JwtRequest import jwt_request
 from ..util import response
 
 _logger = logging.getLogger(__name__)
