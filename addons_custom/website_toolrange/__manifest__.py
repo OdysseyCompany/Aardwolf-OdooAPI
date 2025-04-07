ba501a7047bf6159538a2ac52d4b6f86fc2b7b4a
hieukun1011 <hieukun1011@gmail.com>
2025-04-08 02:43:10 +0700

    update

--- addons_custom/website_toolrange/__manifest__.py
+++ addons_custom/website_toolrange/__manifest__.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
+# Copyright 2024 Wokwy - quochuy.software@gmail.com
 {
     'name': 'Website Toolrange',
-    'version': '17.0.1.0.0',
-    'category': 'Theme/Creative',
-    'summary': 'Theme The Chef is a popular attractive and unique '
-               'front end theme for your restaurant website.',
-    'description': 'Design Web Pages with Theme The Chef',
-    'author': 'Cybrosys Techno Solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'maintainer': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
+    'version': '1.0',
+    'category': 'Website',
+    'summary': '',
+    'author': '',
+    'website': '',
     'depends': ['website_sale', 'portal', 'api_product'],
     'data': [
         'views/website_templates.xml',
@@ -17,17 +13,13 @@
     ],
     'assets': {
         'web.assets_frontend': [
-            # 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
             'website_toolrange/static/src/css/style_home.css',
             'website_toolrange/static/src/css/style_category.css',
         ],
     },
-    'images': [
-        'static/description/banner.jpg',
-        'static/description/theme_screenshot.jpg',
-    ],
-    'license': 'LGPL-3',
     'installable': True,
+    'application': True,
     'auto_install': False,
-    'application': False,
+    'translations': [],
 }
