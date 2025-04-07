6e026235639b8b1a3f22bf6415570fa975572ffb
hieukun1011 <hieukun1011@gmail.com>
2025-04-08 03:04:18 +0700

    update

--- addons_custom/website_aardwolf/__manifest__.py
+++ addons_custom/website_aardwolf/__manifest__.py
@@ -9,13 +9,10 @@
     'data': [
         'views/website_templates.xml',
         'views/website_templates_categories.xml',
-        # 'views/website_templates_product.xml',
+        'views/website_templates_product.xml',
     ],
     'assets': {
         'web.assets_frontend': [
-            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
-            'website_aardwolf/static/src/css/style_home.css',
-            'website_aardwolf/static/src/css/style_category.css',
         ],
     },
     'installable': True,
