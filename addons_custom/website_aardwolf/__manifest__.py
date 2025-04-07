104ebc4463c020a93ebb99215660a51a7a7fd86a
hieukun1011 <hieukun1011@gmail.com>
2025-04-08 02:22:50 +0700

    update

--- addons_custom/website_aardwolf/__manifest__.py
+++ addons_custom/website_aardwolf/__manifest__.py
@@ -1,29 +1,23 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'Website Aardwolf',
-    'version': '17.0.1.0.0',
-    'category': 'Theme/Creative',
-    'summary': 'Theme The Chef is a popular attractive and unique '
-               'front end theme for your restaurant website.',
-    'description': 'Design Web Pages with Theme The Chef',
-    'author': 'Cybrosys Techno Solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'maintainer': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
+    'name': 'Website Toolrange',
+    'version': '18.0.1.0.0',
+    'category': 'Website',
+    'summary': '',
+    'description': '',
+    'author': '',
+    'company': '',
+    'maintainer': '',
+    'website': "",
     'depends': ['website_sale', 'portal', 'api_product'],
     'data': [
         'views/website_templates.xml',
         'views/website_templates_categories.xml',
-        'views/website_templates_product.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
-            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
-            'website_aardwolf/static/css/style.css',
-            'website_toolrange/static/src/js/main.js',
-            'website_aardwolf/static/js/slider.js',
-
+            'website_toolrange/static/src/css/style_home.css',
+            'website_toolrange/static/src/css/style_category.css',
         ],
     },
     'images': [
